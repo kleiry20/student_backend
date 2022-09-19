766a13fad05d7c419eb77ce5a373e8d59f96348c
kleiry20 <anushkasingh801@gmail.com>
2022-09-19 17:07:38 +0530

    standard_name added to maps

--- student_class_maps/serializers.py
+++ student_class_maps/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import StudentClassMap
 
+
 class StudentClassMapSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
+    standard_name = serializers.CharField(source='stdID.stdName', read_only=True)
 
     class Meta:
         model = StudentClassMap
-        fields = ['id', 'stdID', 'studID', 'marks', 'rank']   
+        fields = ['id', 'stdID', 'studID','standard_name', 'marks', 'rank']   
